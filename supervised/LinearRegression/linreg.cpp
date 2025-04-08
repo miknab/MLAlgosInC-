fc07ae4ef218f89b4373d09f323484a1a1dc43ef
Mischa Knabenhans <mischak@physik.uzh.ch>
2025-04-08 15:28:29 +0200

    Initial skeleton for linear regression code

---
+++ supervised/LinearRegression/linreg.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void load_data(){}
+
+void train_test_split(){}
+
+class LinearRegressionModel{
+	private:
+	public:
+	void fit(){}
+	void predict(){}
+}
+
+int main(){
+	return 0;
+}
